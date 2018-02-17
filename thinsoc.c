e87214517867c11968940e6e961991e3669238a0
Nicholas Peretti <qwertyflagstop@Nicholass-MacBook-Pro.local>
2018-02-17 14:05:45 -0500

    inital c imports can build

---
+++ thinsoc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+
+
+int main(int argc, char **argv) 
+{
+    //initial commit!
+}
