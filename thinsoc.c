6f37ba9fddb7978aa23962c333c99d7da01c1be1
Nicholas Peretti <qwertyflagstop@Nicholass-MacBook-Pro.local>
2018-02-17 17:49:29 -0500

    the threads

--- thinsoc.c
+++ thinsoc.c
@@ -7,8 +7,54 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 #include <errno.h>
+#include <pthread.h>
+#include <string.h>
 
 #define PORT 668
+
+
+//listener
+void *listenSOX(int socketFD)
+{
+    while(1)
+    {
+        int msglen = sizeof(unsigned char)*500;
+        unsigned char *msg = (unsigned char *)malloc(msglen);
+        int lengthWeGot = recv(socketFD, msg, msglen, 0);
+        if (lengthWeGot>0)
+        {
+            printf("l %i sock got %s\n", lengthWeGot, msg);//fiz later
+        }
+        free(msg);
+        usleep(1000);
+    }
+    return NULL;
+}
+
+int sendSOX(unsigned char *msg, unsigned char *from, int socketFD)
+{
+    int sizeOfPayload = sizeof(unsigned char)*8+strlen(msg)+1;
+    unsigned char *payload = (unsigned char *)malloc(sizeOfPayload); //SOX | NAME0 | MSG...
+    memcpy(payload, "SOX",3);
+    memcpy(payload+3, from, strlen(from));
+    memcpy(payload+3+strlen(from), msg, strlen(msg));
+    printf("SENDING %i : %s \n", sizeOfPayload, payload);
+    int totalBytesSent=0;
+    for (int i = 0; i < 254; ++i)
+    {
+        char *destinationIP = (char*)malloc(15 * sizeof(char));
+        sprintf(destinationIP, "192.168.1.%i", i);
+        struct sockaddr_in dest;
+        dest.sin_family = AF_INET;
+        dest.sin_addr.s_addr = inet_addr(destinationIP);
+        dest.sin_port = htons(PORT);
+        totalBytesSent += sendto(socketFD, payload, sizeOfPayload, 0, (struct sockaddr *)&dest, sizeof(dest));
+        free(destinationIP);
+    }
+    free(payload);
+    return totalBytesSent;
+}
+
 
 int main(int argc, char **argv) 
 {
@@ -47,35 +93,25 @@ int main(int argc, char **argv)
 
     int count = 0;
     int blasts = 0;
+
+    pthread_t listener_thread;
+    if(pthread_create(&listener_thread, NULL, listenSOX, server_fd)) {
+        fprintf(stderr, "Error creating thread\n");
+        exit(EXIT_FAILURE);
+    }
+
+    char *iname = (unsigned char *)malloc(sizeof(unsigned char)*5);
+    printf("What is you name?(5 characters max): ");
+    fgets(iname, 5, stdin);
+    char *name = (unsigned char *)malloc(sizeof(unsigned char)*5);
+    memcpy(name,iname,5);
+
     while(1)
     {
-        char *destinationIP = (char*)malloc(15 * sizeof(char));
-        sprintf(destinationIP, "192.168.1.%i", count%253);
-        struct sockaddr_in dest;
-        dest.sin_family = AF_INET;
-        dest.sin_addr.s_addr = inet_addr(destinationIP);
-        dest.sin_port = htons(PORT);
-        char *payload = "HELLOHEL";
-        int bytesSent = sendto(server_fd, payload, sizeof(payload), 0, (struct sockaddr *)&dest, sizeof(dest));
-        printf("%i sent\n",bytesSent);
-        printf("ERROR: %s\n", strerror(errno));
-        int msglen = sizeof(unsigned char)*100;
-        unsigned char *msg = (unsigned char *)malloc(msglen);
-        // int lengthWeGot = recv(server_fd, msg, msglen, 0);
-        // if (lengthWeGot>0)
-        // {
-        //     printf("%s\n", msg);
-        // } else {
-        //     printf("we got nothing\n");
-        // }
-        free(msg);
-        free(destinationIP);
-        count+=1;
-        if (count%253==0)
-        {
-            blasts +=1 ;
-            printf("blasted %i\n",blasts);
-            sleep(1);
-        }
+        //input
+        unsigned char *msg = "This is a sample message that someone might send (:";
+        unsigned char *name = "Bobby";
+        sendSOX(msg, name, server_fd);
+        sleep(1);
     }
 }
